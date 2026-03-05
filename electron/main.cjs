f6be7f377578a1f7967d0a3045bea7aada6ae778
Wilfred Guillen <wilfre108199@gmail.com>
2026-03-05 05:47:37 -0500

    chore: add electron packaging for Windows setup

---
+++ electron/main.cjs
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const isDev = Boolean(process.env.VITE_DEV_SERVER_URL)
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 680,
+    autoHideMenuBar: true,
+    backgroundColor: '#0b1220',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  })
+
+  if (isDev) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+    win.webContents.openDevTools({ mode: 'detach' })
+  } else {
+    win.loadFile(path.join(__dirname, '..', 'dist', 'index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
